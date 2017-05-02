df4ad750b075ab6c9b8ec0880e75342c23104399
Andreas Rossholm <arosshol@microsoft.com>
2017-05-02 19:32:21 +0200

    egg comments

--- egg_problem.py
+++ egg_problem.py
@@ -6,18 +6,22 @@ def eggDrop(n, k):
     # A 2D table where entery eggFloor[i][j] will represent minimum
     # number of trials needed for i eggs and j floors.
     eggFloor = [[0 for x in range(k + 1)] for x in range(n + 1)]
-
-    # We need one trial for one floor and0 trials for 0 floors
+    print("eggFloor")
+    print(eggFloor)
+    # We need one trial for one floor and 0 trials for 0 floors
     for i in range(1, n + 1):
         eggFloor[i][1] = 1
         eggFloor[i][0] = 0
-
+    print("eggFloor2")
+    print(eggFloor)
     # We always need j trials for one egg and j floors.
     for j in range(1, k + 1):
         eggFloor[1][j] = j
-
+    print("eggFloor3")
+    print(eggFloor)
     # Fill rest of the entries in table using optimal substructure
     # property
+    # eggFloor[egg(i)][floor(j)]
     for i in range(2, n + 1):
         for j in range(2, k + 1):
             eggFloor[i][j] = INT_MAX
@@ -25,10 +29,12 @@ def eggDrop(n, k):
                 res = 1 + max(eggFloor[i - 1][x - 1], eggFloor[i][j - x])
                 if res < eggFloor[i][j]:
                     eggFloor[i][j] = res
+                print (eggFloor)
 
     # eggFloor[n][k] holds the result
+    print("eggFloor Resultat")
     print (eggFloor)
     return eggFloor[n][k]
 
 
-print(eggDrop(2,100))
+print(eggDrop(2,10))
